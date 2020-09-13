d33922b8173e0bb5e08129e7f80e4249aac86aa4
cemalahmet <cemal.alicioglu@ug.bilkent.edu.tr>
2020-09-14 00:33:07 +0300

    python solutions

---
+++ EIGHTS.py
@@ -0,0 +1,5 @@
+t = int(input())
+
+for _ in range(t):
+	k = int(input())
+	print(250 * k - 58)
---
+++ JULKA.py
@@ -0,0 +1,5 @@
+for _ in range(10):
+	n = int(input())
+	m = int(input())
+	print((n+m)//2)
+	print((n-m)//2)
